4129774f38bddf2522d56b96d4fa81fe26738319
Aron Fyodor Asor <aronasorman@gmail.com>
2017-05-23 19:37:40 -0700

    Simplify Worker backend by using concurrent.futures.

--- src/barbequeue/worker/backends/inmem.py
+++ src/barbequeue/worker/backends/inmem.py
@@ -1,23 +1,51 @@
 from concurrent.futures import ThreadPoolExecutor
 
+from barbequeue.messaging.classes import SuccessMessage
 from barbequeue.worker.backends.base import BaseBackend
 
 
 class Backend(BaseBackend):
-    def start_reporter(self):
-        pass
+    def __init__(self, *args, **kwargs):
+        # Internally, we use conncurrent.future.Future to run and track
+        # job executions. We need to keep track of which future maps to which
+        # job they were made from, and we use the job_future_mapping dict to do
+        # so.
+        self.job_future_mapping = {}
+        super(Backend, self).__init__(*args, **kwargs)
 
     def schedule_job(self, job):
         """Manually schedule a job given by job. A simple proxy to the Monitor
         Thread's self.start_job command."""
         l = job.get_lambda_to_execute()
-        self.workers.submit(l)
+
+        future = self.workers.submit(l)
+        # assign the futures to a dict, mapping them to a job
+        self.job_future_mapping[future] = job
+        # callback for when the future is now!
+        future.add_done_callback(self.handle_finished_future)
+
+        return future
 
     def shutdown(self, wait=True):
         self.workers.shutdown(wait=wait)
 
     def start_workers(self, num_workers):
         return ThreadPoolExecutor(max_workers=num_workers)
+
+    def handle_finished_future(self, future):
+        job = self.job_future_mapping[future]
+
+        try:
+            result = future.result()
+        except Exception as e:
+            self.report_error(job, e)
+            return
+
+        self.report_success(job, result)
+
+    def report_success(self, job, result):
+        msg = SuccessMessage(job.job_id, result)
+        self.msgbackend.send(self.outgoing_message_mailbox, msg)
 
 
 class Reporter(object):
--- tests/worker/backends/test_inmem.py
+++ tests/worker/backends/test_inmem.py
@@ -4,12 +4,14 @@ import pytest
 
 from barbequeue.common.classes import Job
 from barbequeue.messaging.backends.inmem import Backend
+from barbequeue.messaging.classes import MessageType
 from barbequeue.worker.backends import inmem
 
 
 @pytest.fixture
 def mailbox():
     return "pytest"
+
 
 @pytest.fixture
 def msgbackend():
@@ -22,21 +24,36 @@ def worker(mailbox, msgbackend):
     yield b
     b.shutdown()
 
-@pytest.fixture
-def flag():
-    e = Event()
-    yield e
-    e.clear()
-
 
 def set_flag(threading_flag):
     threading_flag.set()
 
 
 class TestWorker:
-    def test_schedule_job_runs_job(self, worker, flag):
-        job = Job(set_flag.__name__)
+    def test_schedule_job_runs_job(self, worker):
+        flag = Event()
+        job = Job(set_flag, flag)
         worker.schedule_job(job)
 
-        assert False
-        assert flag.wait()
+        assert flag.wait(timeout=2)
+
+    def test_schedule_job_sends_message_on_success(self, worker, mocker):
+        mocker.spy(worker.msgbackend, 'send')
+
+        # this job should never fail.
+        job = Job(id, 9)
+        future = worker.schedule_job(job)
+        # wait for the result to finish
+        future.result()
+
+        # verify that we sent a message through our backend
+        assert worker.msgbackend.send.call_count == 1
+
+        # verify that we passed in a success message,
+        # that includes the job_id, and the result
+        call_args = worker.msgbackend.send.call_args
+        message = call_args[0][1]
+        assert message.type == MessageType.JOB_COMPLETED
+        assert message.message['result'] == future.result()
+        # verify that we're sending the job_id in the message
+        assert message.message['job_id'] == job.job_id
