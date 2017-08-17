b939276652cec0a0d14b109ca8f9d964c3d8a820
Aron Fyodor Asor <aronasorman@gmail.com>
2017-08-17 12:20:24 -0700

    move usercancelled exception to exceptions.py

--- src/barbequeue/exceptions.py
+++ src/barbequeue/exceptions.py
@@ -1,3 +1,6 @@
+from concurrent.futures import CancelledError
+
+
 class BaseError(Exception):
     """
     The base exception for all errors raised by barbequeue
@@ -8,3 +11,12 @@ class TimeoutError(BaseError):
     """
     An error raised by barbequeue.storage.wait() when no job updates come in.
     """
+
+
+class UserCancelledError(CancelledError):
+    """
+    An error raised when the user cancels the current job.
+    """
+
+    def __init__(self, last_stage):
+        self.last_stage = last_stage
